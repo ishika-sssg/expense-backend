c70050de385b44be73bdeb508a2ff2711c0e3b2b
Ishika Gupta <ishikagupta@SSSGITs-MacBook-Air.local>
2024-08-28 16:27:33 +0530

    modified dsn

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -15,11 +15,12 @@ func Connect() {
 	//    connectfunction opens the connection to database :
 	// dsn := "root:Pass@123@tcp(localhost:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local"
 	// d, err := gorm.Open("mysql", "root:Pass@123@tcp(localhost:3306)/mydb?charset=utf8mb4&parseTime=True&loc=Local")
+	dsn := `admin:ishika1234@tcp(database-ig.cz2y4ww0yeis.eu-north-1.rds.amazonaws.com:3306)/mydatabase?charset=utf8mb4&parseTime=True&loc=Local`
 
 	var err error
-	DB, err = gorm.Open("mysql", "root:Pass@123@tcp(localhost:3306)/mydb?parseTime=true")
+	// DB, err = gorm.Open("mysql", "root:Pass@123@tcp(localhost:3306)/mydb?parseTime=true")
+	DB, err = gorm.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
-	// DB = d
 }
